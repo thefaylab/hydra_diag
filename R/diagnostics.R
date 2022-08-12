3c9f40f3ec07856cadb6a56fb0d03ec88feed604
gavinfay <gfay42@gmail.com>
2022-08-12 16:46:56 -0400

    adjusts diagnostics.R to deal with fluid project directory and revised dir structure

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -3,27 +3,40 @@
 #  Last update 03/08/2022
 #  Maria Cristina Perez
 
-rm(list = ls())
+#rm(list = ls())  #Restart R session instead to properly clean your workspace
 
 #change directory
-setwd("C:/Users/macristina.perez/Documents/GitHub/hydra_diag/Diagnostics")
+#setwd("C:/Users/macristina.perez/Documents/GitHub/hydra_diag/Diagnostics")
 library(tidyverse)
-source("read.report.R")
-output<-reptoRlist("hydra_sim.rep")# read hydra rep file
-names(output)
-head(output)
+library(ggforce)
+
+
+source("R/read.report.R")
+
+# input & output files to use (these should be arguments in the function that calls this script)
+repfile <- "test-data/hydra_sim.rep"
+tsfile <- "test-data/hydra_sim_NOBA-ts.dat"
+
+
+# read in output files
+# todo - add output directory as an argument to the function that calls this script
+# for now, read in test output
+output<-reptoRlist(repfile)# read hydra rep file
+#names(output)
+#head(output)
 
 #Ncatch_obs 720
 #Ncatch_size_obs 608
 #Nsurvey_size_obs 1680
 #Ndietprop_obs 4721
 #catch_size_pred 1855
+# todo - read in dimensions from data file directly
 
 #### READ OBSERVED (.dat) AND ESTIMATED (.rep) SURVEY VALUES ####
 # careful with "skip" and "nrows" value!
-
-obs_survey<-read.table("hydra_sim_NOBA-ts.dat", skip=1695, nrows=1680, header=F)
-colnames(obs_survey) <- c('number','year','species','type','InpN','1','2','3','4','5')
+# todo - add input data file / directory as an argument to the function that calls this script
+obs_survey<-read.table(tsfile, skip=1695, nrows=1680, header=F)
+colnames(obs_survey) <- c('number','year','species','type','InpN','1','2','3','4','5')  #todo - modify so number of bins is based on size of dataframe
 obs_survey<-obs_survey %>% pivot_longer(cols=6:10, names_to = "lenbin") %>%
   filter(value != -999)%>%
   mutate(lenbin = as.integer(str_remove(lenbin, "V")))
@@ -42,7 +55,7 @@ colnames(obs_survey) <- c('number','year','species','type','InpN','lenbin','obs_
 
 #### READ OBSERVED (.dat) AND PREDICTED (.rep) CATCH VALUES ####
 
-obs_catch<-read.table("hydra_sim_NOBA-ts.dat", skip=4109, nrows=608, header=F)
+obs_catch<-read.table(tsfile, skip=4109, nrows=608, header=F)
 colnames(obs_catch) <- c('number','area','year','species','type','InpN','1','2','3','4','5')
 obs_catch<-obs_catch %>% pivot_longer(cols=7:11, names_to = "lenbin") %>%
   mutate(lenbin = as.integer(str_remove(lenbin, "V"))) %>%
@@ -60,11 +73,12 @@ colnames(obs_catch) <- c('number','area','year','species','type','InpN','lenbin'
                          'pred_value','nll','pearson')
 
 
-diet_catch<-full_join(obs_catch, obs_survey, by = NULL,copy = FALSE)
+#diet_catch<-full_join(obs_catch, obs_survey, by = NULL,copy = FALSE)
+diet_catch <- bind_rows(obs_catch, obs_survey)
 
 #### READ OBSERVED (.dat) AND PREDICTED (.rep) DIET PROPORTION VALUES ####
-
-obs_diet<-read.table("hydra_sim_NOBA-ts.dat", skip=4724, nrows=4721, header=F)
+# todo as above, make the data file an argument and have the dimensions be determined from the file rather than hard-coded
+obs_diet<-read.table(tsfile, skip=4724, nrows=4721, header=F)
 #obs_diet$label<-rep(("diet"),each=4721)
 obs_diet<-obs_diet %>% pivot_longer(cols=6:17, names_to = "lenbin") %>%
   mutate(lenbin = as.integer(str_remove(lenbin, "V"))) %>%
@@ -80,30 +94,30 @@ obs_diet$pearson<-((obs_diet$value-obs_diet$pred_diet)/sqrt(obs_diet$pred_diet))
 colnames(obs_diet) <- c('number','year','species','lenbin','InpN','prey','obs_value','label', 'pred_value','nll','pearson')
 
 
-mydata<-full_join(diet_catch, obs_diet, by = NULL,copy = FALSE)
+#mydata<-full_join(diet_catch, obs_diet, by = NULL,copy = FALSE)
+mydata <- bind_rows(diet_catch, obs_diet)
+
+
 mydata$prey[is.na(mydata$prey)] <- -99 # remove 99?s
 mydata$area[is.na(mydata$area)] <- 1
 mydata$type[is.na(mydata$type)] <- 0
 
 # save data frame with observed and predicted values
-# write.csv(mydata, file = "mydata.csv", row.names = T)
+write.csv(mydata, file = "outputs/mydata.csv", row.names = T)
 
 
 #### PLOTS ####
 # 2 options --> read the csv file saved in the previous steps
 #           --> or continue with mydata data frame
 
-#rm(list = ls())
-setwd("C:/Users/macristina.perez/Documents/GitHub/hydra_diag/Diagnostics")
-data<-read.csv("mydata.csv", header = T)
+#setwd("C:/Users/macristina.perez/Documents/GitHub/hydra_diag/Diagnostics")
+data <- read.csv("outputs/mydata.csv", header = T)
 #data<- select(data, -X)
 data$residual<-ifelse(data$pearson<0,"negative","positive")
 data$res_abs<-abs(data$pearson)
 data<-as.data.frame(data)
-head(data)
+#head(data)
 
-library(tidyverse)
-library(ggplot2)
 names(data)
 str(data)
 
@@ -120,27 +134,28 @@ long_catch <- temp.catch %>%
                names_to = c("kind","junk"),names_sep = "_") %>%
   select(-junk)
 
-sp<-1
+#sp<-1
+plotdir <- "outputs/figures/comp_plots/by_species/"
 
 especies<-unique(long_catch$species)
 for (sp in especies) {
-  
+
   temp_size<-long_catch%>% filter(species == sp) %>%
     group_by(year) %>%
     summarize(mu_ss=mean(InpN))
-  
+
   plot_catch<- long_catch %>% filter (species==sp) %>%
     ggplot() +
-    aes(x=lenbin, y = value) + 
+    aes(x=lenbin, y = value) +
     geom_line(aes(col = kind)) +
     facet_wrap(~year, dir="v") +
     geom_text(data=temp_size, aes(x = 4.8, y = 0.5, label = mu_ss), size=3) +
     theme(legend.position = "bottom") +
     labs(col="") +
     guides(col = guide_legend(nrow = 1))
-  
-    ggsave(paste0("complot_catch_",sp,".jpeg"), plot_catch, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
-  
+
+    ggsave(paste0(plotdir,"complot_catch_",sp,".jpeg"), plot_catch, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
+
 }
 
 #### plot 1 length composition plots by species (survey) ####
@@ -161,19 +176,21 @@ for (sp in especies) {
 
   plot_surv<- long_surv %>% filter (species==sp& number==1) %>%
     ggplot() +
-    aes(x=lenbin, y = value) + 
+    aes(x=lenbin, y = value) +
     geom_line(aes(col = kind)) +
     facet_wrap(~year, dir="v") +
     geom_text(data=temp_size, aes(x = 4.5, y = 0.5, label = mu_ss), size=3) +
     theme(legend.position = "bottom") +
     labs(col="") +
     guides(col = guide_legend(nrow = 1))
-  
-  ggsave(paste0("complot_surv_",sp,".jpeg"), plot_surv, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
+
+  ggsave(paste0(plotdir,"complot_surv_",sp,".jpeg"), plot_surv, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
 
 }
 
 #### plot 2 length-comp plots by species aggregated by year (catch and survey) ####
+
+plotdir <- "outputs/figures/comp_plots/by_year/"
 
 ### catch
 
@@ -238,7 +255,7 @@ complot_year<- long_temp2 %>%
   labs(col="") +
   guides(col = guide_legend(nrow = 1))
 
-ggsave(("complot_year_catch.jpeg"), complot_year)#, width=3000, height=2500, res=250)
+ggsave(paste0(plotdir,"complot_year_catch.jpeg"), complot_year)#, width=3000, height=2500, res=250)
 
 
 ### survey
@@ -303,14 +320,15 @@ complot_year<- long_temp2 %>%
   labs(col="") +
   guides(col = guide_legend(nrow = 1))
 
-ggsave(("complot_year_survey.jpeg"), complot_year)#, width=3000, height=2500, res=250)
+ggsave(paste0(plotdir,"complot_year_survey.jpeg"), complot_year)#, width=3000, height=2500, res=250)
 
 
 #### plot 3 pearson residuals bubble plot ####
+plotdir <- "outputs/figures/residuals/"
 
 ### catch, species= 1 to 11
 
-tiff("bubbleplot_catch.jpeg", width=3000, height=2500, res=250)
+tiff(paste0(plotdir,"bubbleplot_catch.jpeg"), width=3000, height=2500, res=250)
 ggplot(temp.catch, aes(x=year, y=lenbin, size = res_abs, color=factor(residual))) +
   geom_point(alpha=0.7) + theme(title = element_text(angle = 0, hjust = 0.5, size=15, colour="black")) +
   facet_wrap(.~species) + labs(x="year", y="length bin", title="Pearson residuals")
@@ -318,7 +336,7 @@ dev.off()
 
 ### survey, species = 1 to 11
 
-tiff("bubbleplot_survey.jpeg", width=3000, height=2500, res=250)
+tiff(paste0(plotdir,"bubbleplot_survey.jpeg"), width=3000, height=2500, res=250)
 ggplot(temp.surv, aes(x=year, y=lenbin, size = res_abs, color=factor(residual))) +
   geom_point(alpha=0.7) + theme(title = element_text(angle = 0, hjust = 0.5, size=15, colour="black")) +
   facet_wrap(.~species) + labs(x="year", y="length bin", title="Pearson residuals")
@@ -326,6 +344,7 @@ dev.off()
 
 
 #### plot 4 proportions of stomach weights for each prey given a predator size class ####
+plotdir <- "outputs/figures/diet/"
 
 predicted<- as.data.frame(cbind(temp.diet$number, temp.diet$year, temp.diet$species,
                                 temp.diet$lenbin, temp.diet$pred_value, temp.diet$prey))
@@ -342,7 +361,6 @@ observed$sizefit<- paste0(observed$lenbin,".",observed$type2)
 pred_obs <- bind_rows(predicted, observed)# %>%
   #mutate(sizefit = paste0(lenbin,".",type2))
 
-library(ggforce)
 
 ### species = 1
 
@@ -378,16 +396,17 @@ for (sp in especies) {
     scale_fill_brewer(type = "qual", palette = 3)
   # facet_wrap_paginate(~ , ncol = 4, nrow = 5, page = 4)
 
-  ggsave(paste0("diet_plot_",sp,".jpeg"), plot_diet, width = 10, height = 9, dpi = 300)#, width=3000, height=2500, res=250)
+  ggsave(paste0(plotdir,"diet_plot_",sp,".jpeg"), plot_diet, width = 10, height = 9, dpi = 300)#, width=3000, height=2500, res=250)
 
 }
 
 #### plot 5 survey sample size plots ####
+plotdir <- "outputs/figures/"
 
-obs_survey<-read.table("hydra_sim_NOBA-ts.dat", skip=1695, nrows=1680, header=F)
+obs_survey<-read.table(tsfile, skip=1695, nrows=1680, header=F)
 colnames(obs_survey) <- c('number','year','species','type','InpN','1','2','3','4','5')
 
-predicted_survey<-read.table("hydra_sim.rep", skip=17618, nrows=1680, header=F)
+predicted_survey<-read.table("test-data/hydra_sim.rep", skip=17618, nrows=1680, header=F)
 colnames(predicted_survey) <- c('number','year','species','data','cv','predicted','residual','nll')
 head(predicted_survey)
 
@@ -397,48 +416,48 @@ names(predicted_survey)
 
 sp<-1
 
-especies<-unique(predicted_survey$species)
-for (sp in especies) {
-  
-  plot_size<- predicted_survey %>% filter (predicted_survey$number==1) %>%
+# especies<-unique(predicted_survey$species)
+# for (sp in especies) {
+for (isurvey in unique(predicted_survey$number)) {
+
+  plot_size <- predicted_survey %>% filter(number==isurvey) %>%
     ggplot() +
-    aes(x=nll, y = InpN) +
+    aes(x=nll, y = InpN) +  # todo: should be InpN vs EffN
     geom_point() +
-    facet_wrap(~species) 
-    
-  ggsave(paste0("samplesize_survey_",sp,".jpeg"), plot_size, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250) 
-  
-} 
+    facet_wrap(~species)
+
+  ggsave(paste0(plotdir,"samplesize_survey_",isurvey,".jpeg"), plot_size, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
+
+}
 
 
 #### plot 5 catch sample size plots ####
 ### still working on this
 
-obs_catch<-read.table("hydra_sim_NOBA-ts.dat", skip=4109, nrows=608, header=F)
+obs_catch<-read.table(tsfile, skip=4109, nrows=608, header=F)
 colnames(obs_catch) <- c('number','area','year','species','type','InpN','1','2','3','4','5')
 
-predicted_catch<-read.table("hydra_sim.rep", skip=17618, nrows=1680, header=F)
-colnames(predicted_survey) <- c('number','year','species','data','cv','predicted','residual','nll')
-head(predicted_survey)
+predicted_catch<-read.table(repfile, skip=17618, nrows=1680, header=F)
+colnames(predicted_catch) <- c('number','year','species','data','cv','predicted','residual','nll')
 
-predicted_survey$InpN<-obs_survey$InpN
-names(predicted_survey)
+predicted_catch$InpN<-obs_catch$InpN
+names(predicted_catch)
 
 
 sp<-1
 
-especies<-unique(predicted_survey$species)
-for (sp in especies) {
-  
-  plot_size<- predicted_survey %>% filter (predicted_survey$number==1) %>%
+#especies<-unique(predicted_survey$species)
+for (ifleet in unique(predicted_catch[,1])) {
+
+  plot_size<- predicted_catch %>% filter(number==ifleet) %>%
     ggplot() +
-    aes(x=nll, y = InpN) +
+    aes(x=nll, y = InpN) +   # todo: should be InpN vs EffN
     geom_point() +
-    facet_wrap(~species) 
-  
-  ggsave(paste0("samplesize_survey_",sp,".jpeg"), plot_size, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250) 
-  
-} 
+    facet_wrap(~species)
+
+  ggsave(paste0(plotdir,"samplesize_catch_",ifleet,".jpeg"), plot_size, width = 10, height = 7, dpi = 300)#, width=3000, height=2500, res=250)
+
+}
 
 
 
