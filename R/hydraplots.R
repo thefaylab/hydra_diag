d83b9a585748643be7e303b3b45c654e04f30d56
gavinfay <gfay42@gmail.com>
2022-09-12 12:34:25 -0400

    adds hydraplots() function to call the diagnostics Rmd given a data object and report file.

---
+++ R/hydraplots.R
@@ -0,0 +1,14 @@
+# this generates the html output of diagnostics for a Hydra run given a data object and the report file
+hydraplots <- function(data, report, outfile = "../outputs/junk.html") {
+  #data_object = "../test-data/hydraDataList.rda"
+  data_object = data
+  #repfile <- "../test-data/hydra_sim.rep"
+  repfile <- repfile
+  #outfile = "../outputs/junk.html"
+  rmarkdown::render(input = "R/hydra-diagnostics.Rmd",
+                    output_file = outfile)
+}
+#example call
+hydraplots(data = "../test-data/hydraDataList.rda",
+           report = "../test-data/hydra_sim.rep",
+           outfile = "../outputs/junk.html")
